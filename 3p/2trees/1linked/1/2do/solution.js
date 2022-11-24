1c45e4f61ddb5211627ca18d435904c044bff6a1
Jorge Torres <104809141+Jorge-Antonio-Torres-Lozoya@users.noreply.github.com>
2022-11-24 10:32:33 +0000

    commit

--- 3p/2trees/1linked/1/2do/solution.js
+++ 3p/2trees/1linked/1/2do/solution.js
@@ -1,8 +1,8 @@
 
-import {BinarySearchTree} from '../../../../../jatl/tree/tree6es.js'
+import { BinarySearchTree } from '../../../../../jatl/tree/tree6esPreOrder.js'
 
- let tree = new BinarySearchTree()
-  tree.insert(4)
-  tree.insert(2)
-  console.log('PreOrden');
-  tree.preorder()
+let tree = new BinarySearchTree()
+tree.insert(4)
+tree.insert(2)
+console.log('PreOrden');
+tree.preorder()
