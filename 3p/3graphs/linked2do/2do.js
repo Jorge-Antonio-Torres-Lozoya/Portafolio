0701fdea42bcbbe4d10a107380d74491739d1d43
Jorge Torres <104809141+Jorge-Antonio-Torres-Lozoya@users.noreply.github.com>
2022-11-24 10:28:33 +0000

    commit

--- 3p/3graphs/linked2do/2do.js
+++ 3p/3graphs/linked2do/2do.js
@@ -2,3 +2,13 @@
 // design node of a 
 // weighted graph incidence list 
 // 
+
+
+
+let incidenseMatrix = {
+  'A': [1, 0, 0, 4, 0, 0],
+  'B': [1, 2, 0, 0, 5, 6],
+  'C': [0, 2, 3, 0, 0, 0],
+  'D': [0, 0, 3, 4, 5, 6]
+}
+console.log(incidenseMatrix);
